1de5e176b400ca7d3c8293d29dcc21f6c640b8ea
NT5 <hendelporras@hotmail.com>
2017-07-02 10:17:34 -0600

    Arreglos al Formato No14. WIP #26

---
+++ res/sql/formats/Formato14.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `Formato_14` (
+  `Reg_Id` int(15) NOT NULL AUTO_INCREMENT,
+  `Ped_Id` int(15) NOT NULL,
+  `Carrera_Id` int(15) NOT NULL,
+  `Anio_Lectivo` int(15) NOT NULL,
+  `Reti_Mujeres_Id` int(15) NOT NULL,
+  `Reti_hombres_Id` int(15) NOT NULL,
+  `Egre_mujeres_Id` int(15) NOT NULL,
+  `Egre_hombres_Id` int(15) NOT NULL,
+  `Turno_Id` int(15) NOT NULL,
+  PRIMARY KEY (`Reg_Id`),
+  FOREIGN KEY (`Carrera_Id`) REFERENCES `Carreras` (`Carrera_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`Ped_Id`) REFERENCES `Peds` (`Ped_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`Turno_Id`) REFERENCES `Turnos` (`Turno_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
