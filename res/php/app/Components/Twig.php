299b11cc42cf21e62331067647510d32fd6e590f
NT5 <hendelporras@hotmail.com>
2017-07-12 12:54:30 -0600

    Añadida documentación y ejemplos para el módulo Twig. WIP #7

--- res/php/app/Components/Twig.php
+++ res/php/app/Components/Twig.php
@@ -3,42 +3,48 @@
 namespace REC1\Components;
 
 /**
- * @todo Documentar/Mejorar
+ * Clase principal contenedora de los métodos para manejar plantillas en formato Twig
+ * @link https://twig.sensiolabs.org/doc/1.x/templates.html
  */
 class Twig {
 
     /**
-     *
-     * @var string 
+     * 
+     * @var string Ubicación del archivo de la plantilla Twig que esta ubicado en $Twig_Template_Folder por defecto es base.twig
+     * @see $Twig_Template_Folder
      */
     private $Template_File;
 
     /**
-     *
-     * @var string 
+     * 
+     * @var string Carpeta base contenedora de todas las plantillas Twig. por defecto esta ubicado en la carpeta twig
      */
     private $Twig_Template_Folder;
 
     /**
      *
-     * @var array 
+     * @var array Set de Variables manejadas y transmitidas a las plantillas Twig
      */
     private $Twig_Vars;
 
     /**
      *
-     * @var \Twig_Loader_Filesystem 
+     * @var \Twig_Loader_Filesystem Componente del módulo Twig que maneja directorios de plantillas
+     * @see https://twig.symfony.com/doc/1.x/api.html
      */
     private $Twig_Loader_Filesystem;
 
     /**
      *
-     * @var \Twig_Environment 
+     * @var \Twig_Environment Componente de módulo Twig que maneja la configuración principal.
+     * @see https://twig.symfony.com/doc/1.x/api.html
      */
     private $Twig_Environment;
 
     /**
-     * 
+     * Función constructora de la clase. Establece todos los valores de configuracion por defecto.
+     * Es necesario crear un objeto antes de ejecutar cualquier método
+     * @example ./docs/examples/Components/Twig.md 2 2 Creacion del objeto Twig
      */
     public function __construct() {
         $this->Twig_Template_Folder = \REC1\Util\Functions::parseDir(array(__DIR__, "..", "..", "..", "twig"));
@@ -56,42 +62,52 @@ class Twig {
     }
 
     /**
-     * 
-     * @param string $template
+     * Establece el archivo que será usado de plantilla en el objeto actual.
+     * El Archivo tiene que estar ubicado en $Twig_Template_Folder.
+     * Solo se puede cargar un archivo por objeto, si quieres cargar más de uno tiene que usar la sintaxis Twig
+     * @see $Twig_Template_Folder
+     * @param string $template Nombre del archivo o ubicación relativa a $Twig_Template_Folder
+     * @example ./docs/examples/Components/Twig.md 7 9 Establecer archivo de plantilla
      */
     public function setTemplate($template) {
         $this->Template_File = $template;
     }
 
     /**
-     * 
-     * @return string
+     * Regresa el valor de $Template_File
+     * @see $Template_File
+     * @return string Archivo o ubicación relativa de la plantilla actual
      */
     public function getTemplate() {
         return $this->Template_File;
     }
 
     /**
-     * 
-     * @param string $filter_name
-     * @param string $filter_function
+     * Añade filtros del módulo Twig
+     * @see https://twig.symfony.com/doc/1.x/filters/index.html
+     * @see https://twig.symfony.com/doc/1.x/advanced.html
+     * @param string $filter_name Nombre del filtro que sera llamado desde las plantillas Twig
+     * @param string $filter_function Funcion PHP que sera invocada en las plantillas Twig
+     * @example ./docs/examples/Components/Twig.md 17 6 Establece un filtro nuevo
      */
     public function addFilter($filter_name, $filter_function) {
         $this->Twig_Environment->addFilter($filter_name, new \Twig_Filter_Function($filter_function));
     }
 
     /**
-     * 
-     * @param string $name
-     * @param string $value
+     * Agrega variables que serán usadas posteriormente en la plantilla Twig
+     * @param string $name Nombre de la variable (Pueden ser multidimensionales separadas por un punto)
+     * @param string|array $value Valor de la variable puede ser un array o cualquier tipo de objeto
+     * @example ./docs/examples/Components/Twig.md 24 10 Añade variables
      */
     public function setVar($name, $value) {
         \REC1\Util\Functions::set_array_value($this->Twig_Vars, $name, $value);
     }
 
     /**
-     * 
+     * Agrega un arreglo de variables a la plantilla
      * @param array $vars
+     * @example ./docs/examples/Components/Twig.md 35 9 Añade variables
      */
     public function setVars(array $vars) {
         foreach ($vars as $k => $v) {
@@ -100,23 +116,31 @@ class Twig {
     }
 
     /**
-     * 
+     * Regresa el valor de $Twig_Vars
      * @return array
+     * @see $Twig_Vars
      */
     public function getVars() {
         return $this->Twig_Vars;
     }
 
     /**
-     * 
-     * @param string $name
+     * Regresa el valor de una variable ubicada en $Twig_Vars.
+     * El nombre puede estar separado por puntos en variables multidimensionales 
+     * @param string $name Indice dentro de $Twig_Vars
+     * @see $Twig_Vars
+     * @example ./docs/examples/Components/Twig.md 45 7 Retorna variable
      */
     public function getVar($name) {
         return \REC1\Util\Functions::get_array_value($this->Twig_Vars, $name);
     }
 
     /**
-     * 
+     * Compila la plantilla Twig y regresa un string listo para ser mostrado
+     * @see $Twig_Environment
+     * @see $Template_File
+     * @see $Twig_Vars
+     * @example ./docs/examples/Components/Twig.md 53 2 Muestra plantilla
      * @return string
      */
     public function getRender() {
