b4100b36ebdef1df66d49b12d1454781762d0822
NT5 <hendelporras@hotmail.com>
2017-08-24 21:09:11 -0600

    Mas cosas

--- res/js/formats_validation.js
+++ res/js/formats_validation.js
@@ -7,18 +7,43 @@ $(function () {
         }
     });
 
+    $('.btn-autofill.autofill-clear').on('click', function () {
+        var controlForm = $('table#table-data tbody');
+
+        controlForm.find('tr:not(:first)').each(function () {
+            $(this).remove();
+        });
+        Materialize.toast('¡Formulario limpiado!', 4000, 'rounded');
+
+        controlForm.find('tr:first .btn-remove')
+                .removeClass('btn-remove').addClass('btn-add')
+                .text('add');
+    });
+
     $('.btn-autofill.autofill-carreras').on('click', function () {
-        var controlForm = $('table#table-data tbody'),
-                currentEntry = controlForm.find('tr:first');
+        var controlForm = $('table#table-data tbody');
 
-        $('table#table-data .input-field select[name="carrera_id[]"] option:eq(0)').prop('selected', true);
+        controlForm.find('tr').each(function () {
+            var currentEntry = $(this);
 
-        var carreras = $('table#table-data .input-field select[name="carrera_id[]"] option:not(:first)');
-        carreras.each(function () {
-            var value_car = $(this).val(),
-                    newEntry = $(currentEntry.clone()).appendTo(controlForm);
-            newEntry.find('select[name="carrera_id[]"]')
-                    .val(value_car);
+            currentEntry
+                    .find('select[name="carrera_id[]"] option:eq(0)')
+                    .prop('selected', true);
+
+            currentEntry.find('.input-field select[name="carrera_id[]"] option:not(:first)').reverse().each(function () {
+                currentEntry.after(currentEntry.clone());
+
+                var newEntry = $(currentEntry).next(),
+                        value_itm = $(this).val();
+
+                newEntry.find('select[name="turno_id[]"]')
+                        .val($(currentEntry).find('select[name="turno_id[]"]').val());
+                newEntry.find('select[name="anio_carrera[]"]')
+                        .val($(currentEntry).find('select[name="anio_carrera[]"]').val());
+
+                newEntry.find('select[name="carrera_id[]"]')
+                        .val(value_itm);
+            });
         });
         Materialize.toast('¡Carreras añadidas!', 4000, 'rounded');
 
@@ -36,34 +61,83 @@ $(function () {
     });
 
     $('.btn-autofill.autofill-turnos').on('click', function () {
-        var controlForm = $('table#table-data tbody'),
-                firstEntry = controlForm.find('tr:first');
-
-        var turnos = (function () {
-            var itmm = [];
-            firstEntry.find('.input-field select[name="turno_id[]"] option').each(function () {
-                itmm.push({
-                    'name': $(this).text(),
-                    'val': $(this).val()
-                });
-            });
-            return itmm;
-        })();
-
-        console.log(turnos);
+        var controlForm = $('table#table-data tbody');
 
         controlForm.find('tr').each(function () {
-            var currentEntryTable = $(this);
-            currentEntryTable.find('.input-field select[name="turno_id[]"] option:not(:first)').each(function () {
-                var newEntry = currentEntryTable.after(currentEntryTable.clone());
+            var currentEntry = $(this);
 
-                console.log(newEntry);
+            currentEntry
+                    .find('select[name="turno_id[]"] option:eq(0)')
+                    .prop('selected', true);
+
+            currentEntry.find('.input-field select[name="turno_id[]"] option:not(:first)').reverse().each(function () {
+                currentEntry.after(currentEntry.clone());
+
+                var newEntry = $(currentEntry).next(),
+                        value_itm = $(this).val();
+
+                newEntry.find('select[name="carrera_id[]"]')
+                        .val($(currentEntry).find('select[name="carrera_id[]"]').val());
+                newEntry.find('select[name="anio_carrera[]"]')
+                        .val($(currentEntry).find('select[name="anio_carrera[]"]').val());
+
+                newEntry.find('select[name="turno_id[]"]')
+                        .val(value_itm);
             });
         });
+        Materialize.toast('¡Turnos añadidas!', 4000, 'rounded');
 
         controlForm.find('tr:not(:last) .btn-add')
                 .removeClass('btn-add').addClass('btn-remove')
                 .text('remove');
+
+        controlForm.find('.input-field input[type=number]').change(function () {
+            var container = $(this);
+            var valor = parseInt(container.val());
+            if (valor) {
+                container.val(Math.abs(valor));
+            }
+        });
+    });
+
+    $('.btn-autofill.autofill-anio_carrera').on('click', function () {
+        var controlForm = $('table#table-data tbody');
+
+        controlForm.find('tr').each(function () {
+            var currentEntry = $(this);
+
+            currentEntry
+                    .find('select[name="anio_carrera[]"] option:eq(0)')
+                    .prop('selected', true);
+
+            currentEntry.find('.input-field select[name="anio_carrera[]"] option:not(:first)').reverse().each(function () {
+                currentEntry.after(currentEntry.clone());
+
+                var newEntry = $(currentEntry).next(),
+                        value_itm = $(this).val();
+
+                newEntry.find('select[name="carrera_id[]"]')
+                        .val($(currentEntry).find('select[name="carrera_id[]"]').val());
+                newEntry.find('select[name="turno_id[]"]')
+                        .val($(currentEntry).find('select[name="turno_id[]"]').val());
+
+                newEntry.find('select[name="anio_carrera[]"]')
+                        .val(value_itm);
+            });
+        });
+        Materialize.toast('¡Años de carrera añadidas!', 4000, 'rounded');
+
+        controlForm.find('tr:not(:last) .btn-add')
+                .removeClass('btn-add').addClass('btn-remove')
+                .text('remove');
+
+        controlForm.find('.input-field input[type=number]').change(function () {
+            var container = $(this);
+            var valor = parseInt(container.val());
+            if (valor) {
+                container.val(Math.abs(valor));
+            }
+        });
     });
 
     $(document).on('click', '.btn-add', function (e) {
